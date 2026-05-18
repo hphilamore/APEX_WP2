4ab1c8c96ac7ada752483e367fc0e76c1f997891
hphilamore <hp12384@bristol.ac.uk>
2026-05-18 13:47:38 +0100

    Comments added to analyse data file

--- analyse_data.py
+++ analyse_data.py
@@ -21,53 +21,97 @@ def main():
     # Separate into dictionary of indiviudal MFCs time series data
     all_data_separate = separate_mfc_data(all_data, mfc_column_names)
 
-    # Dictionary to store computed information for each MFC
+
+    # ---------------------------------------------------------------
+    # Compute/extract parameters for each COD event, for each MFC
+    # ---------------------------------------------------------------
+
+    # Dictionary to store computed/extracted parameters
     mfc_analysis = {
         "Spike delay (days)" : {},
+        "COD" : {},
     }
 
+    # Iterate over MFCs 
     for d in all_data_separate:
         print()
         print(d)
         data = all_data_separate[d]
 
-        # Compute power output as time series
-        data['Power W'] = data["Voltage"] * data["Resistance"]**2
-
+        # ------------------------------
+        # -------- Extract COD ---------
+        # ------------------------------
+        
         # Get date-time index of COD events
         cod_events_idx = data.index[data["COD_event"] == 1]
 
-        # Get index of voltage peaks
-        voltage_peaks_idx = []
+        # Store COD values
+        mfc_analysis["COD"][d] = data.loc[cod_events_idx, "COD"]
 
-        # Get window of voltage data following each COD event
+        # -------------------------------------
+        # -------- Extract Resistance ---------
+        # -------------------------------------
+        
+        pass
+
+        # ------------------------------------------------------
+        # -------- Compute peak power and peak voltage ---------
+        # ------------------------------------------------------
+
+        # Arrays to store index of peak values
+        voltage_peaks_idx = []
+        power_peaks_idx = []
+
+        # Compute power output as time series of Voltage in mV * (R)
+        data['Power W'] = (data["Voltage"]/1000) * (data["Resistance"]*1000)**2
+
+        # Get window of data following each COD event
         for i in range(len(cod_events_idx)):
             start = cod_events_idx[i] 
 
             if i < len(cod_events_idx) - 1:
                 end = cod_events_idx[i+1]
-                # Get voltage data exclusive of stopping value
-                window = data.loc[(data.index >= start) & (data.index < end), 'Voltage']
+                mask = (data.index >= start) & (data.index < end)
 
-            # Last segment of data has no stopping value
+            # Last segment of data 
             else:
-                window = data['Voltage'].loc[start:]
+                mask = (data.index >= start) 
 
-            if len(window) > 0:
-                # Find maximum voltage recorded in this window (voltage peak)
-                if window.notna().any():
-                    peak_idx = window.idxmax()
-                    peak_val = data['Voltage'].loc[peak_idx]
+            window = data.loc[mask]
+
+            # Find peak voltage and power values in this window
+            for parameter, peak_vals in zip(
+                                            ['Voltage', 'Power W'], 
+                                            [voltage_peaks_idx, power_peaks_idx]
+                                            ):
+
+                # Get the window of data for the selected parameter (voltage or power)
+                param = window[parameter]
+
+                # If there is data in the window, compute the max value
+                if param.notna().any():
+                    peak_idx = param.idxmax()
+                    peak_val = param.loc[peak_idx]
                 else:
                     peak_idx = None
                     peak_val = None
                     
-                # print('Voltage peak:', peak_idx, peak_val)
+                print('Peak '+ parameter + ':', peak_idx, peak_val)
 
-                # Store index of voltage peak
-                voltage_peaks_idx.append(peak_idx)
+                # Store index of the peak
+                peak_vals.append(peak_idx)
 
-        # Plot voltage data for each MFC, showing peaks
+            # -------------------------
+            # -------- Energy ---------
+            # -------------------------
+
+            # Find total energy recorded in this window (numerical integral of power wrt time) 
+            t = (window.index - window.index[0]).total_seconds()
+            power = window['Power W'].values
+            energy = np.trapezoid(power, t)
+            energy_vals.append(energy)
+
+        # Plot voltage data, showing peaks
         plot_data(data, 
                   ["Voltage"], 
                   title=d, 
@@ -75,6 +119,10 @@ def main():
                   show_days=True,
                   show_plot=False
                   )
+        
+        # -----------------------------
+        # -------- Peak delay ---------
+        # -----------------------------
 
         # Compute and store delay between COD event and voltage spike
         spike_delay_days = []
@@ -88,19 +136,27 @@ def main():
         spike_delay_days = [round(i, 6) for i in spike_delay_days]
         mfc_analysis["Spike delay (days)"][d] = spike_delay_days
 
-    # Save spike delay analysis to excel file 
+
+
+
+    # ---------------------------------------------------------------
+    # -------- Statistical analysis of MFCs grouped by type ---------
+    # ---------------------------------------------------------------
+
+    # Save spike delay for each MFC for each COD event 
     df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in mfc_analysis["Spike delay (days)"].items()]))  
     df.to_excel("Spike_delay_days.xlsx", index=False)
     print(df)
 
+    # MFC types
+    mfc_groups = { '10*10 AC':{}, 
+                '20*30 AC':{}, 
+                '10*10':{}, 
+                '20*30':{}
+                }
+
     
-    # Group spike delay data by MFC type and compute min, max, mean
-    mfc_groups = {  '10*10 AC':{}, 
-                    '20*30 AC':{}, 
-                    '10*10':{}, 
-                    '20*30':{}
-                    }
-    
+    # Group spike delay data by MFC type and compute min, max, mean for each COD event 
     for i, mfc_type in zip(range(0, df.shape[1], 3), mfc_groups):
 
         # Take columns in groups of 3
@@ -110,10 +166,10 @@ def main():
         mfc_groups[mfc_type]['max'] = group.max(axis=1)
         mfc_groups[mfc_type]['mean'] = group.mean(axis=1)
 
+        event_number = range(len(mfc_groups[mfc_type]['mean']))   
 
-        x = range(len(mfc_groups[mfc_type]['mean']))   # or use datetime index if you have one
-
-        plt.plot(x, mfc_groups[mfc_type]['mean'], label=mfc_type)
+        # Plot spike delay data
+        plt.plot(event_number, mfc_groups[mfc_type]['mean'], label=mfc_type)
 
         # shaded area between min and max
         plt.fill_between(x, mfc_groups[mfc_type]['min'], mfc_groups[mfc_type]['max'], alpha=0.3)
